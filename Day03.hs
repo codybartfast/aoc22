c06e147fb15bf3d67d2d60272908397cec944dc7
John Donnellan <john@codybartfast.com>
2022-12-04 12:30:29 +0000

    refactor Day 3

--- Day03.hs
+++ Day03.hs
@@ -2,30 +2,21 @@ module Day03 (solve) where
 
 import qualified Data.Char as Char
 import qualified Data.List as List
-import qualified Data.Set as Set
+import qualified Data.List.Split as Split
 
 solve input lines = do
     let commonItems = map packCommon lines
     print $ sum $ map priority commonItems
 
-    let groups = group [] lines
+    let groups = Split.chunksOf 3 lines
     print $ sum $ map (priority . groupCommon) groups
 
 packCommon line = do
     let packSize = List.length line `div` 2
-    let items1 = Set.fromList (take packSize line)
-    let items2 = Set.fromList (drop packSize line)
-    pick $ Set.intersection items1 items2
-
-pick = head . Set.toList
+    head $ List.intersect (take packSize line) (drop packSize line)
 
 priority c = do
     let code = Char.ord c
     if code < 97 then code - 38 else code - 96
 
-group :: [[String]] -> [String] -> [[String]]
-group groups [] = groups
-group groups elves = group (take 3 elves:groups) (drop 3 elves)
-
-groupCommon group =
-    pick $ List.foldl1 Set.intersection (map Set.fromList group)
+groupCommon group = head $ List.foldl1 List.intersect group
--- Main.hs
+++ Main.hs
@@ -3,12 +3,12 @@ import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.List.Split as Split
 
-import Day04 (solve)
+import Day03 (solve)
 
 trim = List.dropWhileEnd Char.isSpace . dropWhile Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day04/Input.txt"
+  fileContent <- readFile "input/Day03/Input.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
