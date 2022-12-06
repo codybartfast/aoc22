3edebf5d03aeb428e79fa30a1e64b141cfa26b3d
John Donnellan <john@codybartfast.com>
2022-12-06 07:36:15 +0000

    Day 6, Part Two

--- Day06.hs
+++ Day06.hs
@@ -7,12 +7,12 @@ solve input lines = do
     print 
         $ fours input
         & map (length . List.nub)
-        & takeWhile  (< 4)
+        & takeWhile  (< 14)
         & length
-        & (+ 4)
+        & (+ 14)
 
 fours lst = result
     where
-        fours' rslts (a:b:c:d:rest) = fours' ([a, b, c, d]:rslts) (b:c:d:rest)
+        fours' rslts (a:b:c:d:e:f:g:h:i:j:k:l:m:n:rest) = fours' ([a, b, c, d, e, f, g, h, i, j, k, l, m, n]:rslts) (b:c:d:e:f:g:h:i:j:k:l:m:n:rest)
         fours' rslts _ = rslts
         result = fours' [] lst & reverse
