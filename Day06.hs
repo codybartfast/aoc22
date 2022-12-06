716d3c31d07136ff8e4b32e304968e1bee096202
John Donnellan <john@codybartfast.com>
2022-12-06 08:04:41 +0000

    Day 6 refactor

--- Day06.hs
+++ Day06.hs
@@ -4,15 +4,17 @@ import Data.Function
 import qualified Data.List as List
 
 solve input lines = do
-    print 
-        $ fours input
-        & map (length . List.nub)
-        & takeWhile  (< 14)
-        & length
-        & (+ 14)
+    print $ endOfFirstUniqueN 4 input
+    print $ endOfFirstUniqueN 14 input
 
-fours lst = result
-    where
-        fours' rslts (a:b:c:d:e:f:g:h:i:j:k:l:m:n:rest) = fours' ([a, b, c, d, e, f, g, h, i, j, k, l, m, n]:rslts) (b:c:d:e:f:g:h:i:j:k:l:m:n:rest)
-        fours' rslts _ = rslts
-        result = fours' [] lst & reverse
+endOfFirstUniqueN n list =
+    nwise n list
+    & map (length . List.nub)
+    & takeWhile  (< n)
+    & length
+    & (+ n)
+
+nwise n list =
+    List.tails list
+    & map (List.take n)
+    & List.dropWhileEnd ((< n). length)
