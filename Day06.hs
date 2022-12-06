7efa591277684a1e36e88d43b68b639c0b898ea1
John Donnellan <john@codybartfast.com>
2022-12-06 07:29:31 +0000

    Day 6, Part One

--- Day06.hs
+++ Day06.hs
@@ -1,7 +1,18 @@
 module Day06 (solve) where
 
 import Data.Function
+import qualified Data.List as List
 
 solve input lines = do
-    print (take 3 lines)
-    putStrLn ""
+    print 
+        $ fours input
+        & map (length . List.nub)
+        & takeWhile  (< 4)
+        & length
+        & (+ 4)
+
+fours lst = result
+    where
+        fours' rslts (a:b:c:d:rest) = fours' ([a, b, c, d]:rslts) (b:c:d:rest)
+        fours' rslts _ = rslts
+        result = fours' [] lst & reverse
