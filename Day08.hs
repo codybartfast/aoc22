aa42e06ccc496ba2203c22df7c22a4cd2ae3e235
John Donnellan <john@codybartfast.com>
2022-12-08 07:35:39 +0000

    Day 8, Part One

--- Day08.hs
+++ Day08.hs
@@ -2,6 +2,7 @@ module Day08 (solve) where
 
 import Data.Function
 import Data.List
+import Control.Monad (when)
 
 solve input lines = do
     let trees = labelTrees lines
@@ -9,26 +10,19 @@ solve input lines = do
     let columns = transpose trees
     let flippedColumns = map reverse columns
     let linesOfSight = concat [trees, flippedRows, columns, flippedColumns]
-    -- let visible = map pickVisible flippedRows & concat & nub
     let visible = map pickVisible linesOfSight & concat & nub
-
     print (length visible)
-    print visible
 
 pickVisible :: [((Int, Int), Int)] -> [((Int, Int), Int)]
-pickVisible trees = do
-    (head trees) : (
-        nwise 2 trees
-        & takeWhile (\[(_, h1), (_, h2)] -> h1 < h2)
-        & map (head. tail))
+pickVisible =
+    pick [] (-1)
+    where
+        pick rslts mx [] = rslts
+        pick rslts mx (tree@(_, h):rest) =
+            if h > mx then pick (tree:rslts) h rest else pick rslts mx rest
 
 labelTrees :: [String] -> [[((Int, Int), Int)]]
 labelTrees lines = do
     let labelLine = zip [0..]
     map (zip [0..]) lines 
         & zipWith (\y pairs -> map (\(x, t) -> ((x, y), read [t])) pairs ) [0..]
-
-nwise n list =
-    tails list
-    & map (take n)
-    & dropWhileEnd ((< n). length)
