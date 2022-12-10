42ea73a5a7b1e4dd2b8cc6e2abe52d8a9614bfb3
John Donnellan <john@codybartfast.com>
2022-12-10 06:24:45 +0000

    Day 10

---
+++ Day10.hs
@@ -0,0 +1,7 @@
+module Day10 (solve) where
+
+import Data.Function
+import Data.List
+
+solve input lines = do
+    print (take 3 lines)
--- Main.hs
+++ Main.hs
@@ -3,12 +3,12 @@ import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.List.Split as Split
 
-import Day09 (solve)
+import Day10 (solve)
 
 trim = List.dropWhileEnd Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day09/Input.txt"
+  fileContent <- readFile "input/Day10/Input.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
