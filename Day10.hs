4bdeab896cb9fce56b05ea64649da67f47119cb4
John Donnellan <john@codybartfast.com>
2022-12-10 07:06:26 +0000

    Day 10, Part One

--- Day10.hs
+++ Day10.hs
@@ -4,4 +4,22 @@ import Data.Function
 import Data.List
 
 solve input lines = do
-    print (take 3 lines)
+    let program = map readLine lines
+    let states = scanl execute start program
+    let max = states & last & fst
+    let times = [0, 40 .. 200] & map (+ 20) 
+    let atTime n = states & takeWhile (\ (t, _) -> t < n) & last & (\ (t, v) -> (n, v))
+    let calc (t, n) = t * n
+    print $ times & map atTime & map calc & sum
+
+execute :: (Int, Int) -> Maybe Int -> (Int, Int)
+execute (time, x) Nothing = (time + 1, x)
+execute (time, x) (Just v) = (time + 2, x + v)
+
+
+readLine :: String -> Maybe Int
+readLine line = do
+    let (first:rest) = words line
+    if first == "noop" then Nothing else Just $ rest & head & read
+
+start = (0, 1)
