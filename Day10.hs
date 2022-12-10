2d0de281eac73fdf4f2234196eb66168eaf00656
John Donnellan <john@codybartfast.com>
2022-12-10 10:59:17 +0000

    Day 10, Part Two

--- Day10.hs
+++ Day10.hs
@@ -2,35 +2,34 @@ module Day10 (solve) where
 
 import Data.Function
 import Data.List
+import qualified Data.List.Split as Split
 
 solve input lines = do
     let program = map readLine lines
-    let states = scanl execute start program
+    let states = scanl execute (0, 1) program
     let values = getValues states
-    let valueAtTime n = values !! (n - 1)
-    let calc (t, n) = t * n
-    print $  map (calc .valueAtTime) [20, 60 .. 220] & sum
-        
+    let cycleValue c = values !! (c - 1)
+    let interesting (c, n) = c * n
+    print $ map (interesting . cycleValue) [20, 60 .. 220] & sum
+    putStrLn $ 
+        values 
+        & map (\ (c, v) -> if abs ((c - 1) `mod` 40 - v) < 2 then '#' else ' ')
+        & Split.chunksOf 40
+        & concatMap ('\n' :)
+
 getValues states = do
     scanl 
-        (\ states' t -> skipWhile (\ (t', _) -> t' < t ) states')
+        (\ states' c -> dropWhile (\ (c', _) -> c' < c ) states')
         states
-        [0 .. 220]
+        [0 .. 240]
     & map (snd . head)
     & zip [1..]
     
 execute :: (Int, Int) -> Maybe Int -> (Int, Int)
-execute (time, x) Nothing = (time + 1, x)
-execute (time, x) (Just v) = (time + 2, x + v)
+execute (cycle, x) Nothing = (cycle + 1, x)
+execute (cycle, x) (Just v) = (cycle + 2, x + v)
 
 readLine :: String -> Maybe Int
 readLine line = do
     let (first:rest) = words line
     if first == "noop" then Nothing else Just $ rest & head & read
-
-skipWhile :: (a -> Bool) -> [a] -> [a]
-skipWhile _ [] = []
-skipWhile pred list@(hd:tl) = do
-    if pred hd then skipWhile pred tl else list
-
-start = (0, 1)
