376677c753a92b7979b8a652d0af836349fcd623
John Donnellan <john@codybartfast.com>
2022-12-12 13:10:52 +0000

    Day 12 refactor

--- Day12.hs
+++ Day12.hs
@@ -6,56 +6,53 @@ import Control.Monad (forM_)
 import qualified Data.Char as Char
 import qualified Data.Map as Map
 import qualified Data.List.Split as Split
-import GHC.Conc (par)
+import Data.List.NonEmpty (sortWith)
 
 type Terrain = Map.Map (Int, Int) (Int, Int)
 
+-- Elevations are inverted and routes are found from 'E', now the lowest point,
+-- to 'S', now the highest point.  This is so that just one exploration is 
+-- needed for Part Two where 'E' is known but the starting point is not.
+
 solve input lines = do
-    let terrain = readTerrain lines
-    -- showTerrain terrain
-    -- showTerrain $ get terrain (0, 0) & exploreSquare terrain 
+    let coordCharPairs = getCoordCharPairs lines
+    let terrain = readTerrain coordCharPairs
     let terrains = iterate explore terrain
     let finalTerrain =
             terrain
                 & iterate explore
                 & pairwise
-                & partition (\ (a , b) -> a == b)
-                & fst & head & fst
+                & find (uncurry (==))
+                & (\ (Just (trn, _)) -> trn)
 
-    let Just (_, part1) =
-            finalTerrain
-            & Map.lookup (end lines)
-
-    let part2 =
-            (ends lines)
-            & map (snd . snd . get finalTerrain . fst)
-            & minimum
-
-
+    let Just (_, part1) = finalTerrain & Map.lookup (findS coordCharPairs)
     print part1
-    print part2
 
+    print
+        $ findSorA coordCharPairs
+        & map (readMap finalTerrain)
+        & sortOn distance
+        & head
+        & distance
+    
+explore terrain = foldl exploreSquare terrain (Map.keys terrain)
 
-
-explore terrain = do
-    foldl exploreSquare terrain (Map.keys terrain)
-
+exploreSquare :: Terrain -> (Int, Int) -> Terrain
 exploreSquare terrain pos = do
-    let square = get terrain pos
-    let neighbourDist = dist square + 1
+    let square = readMap terrain pos
+    let neighbourDist = distance square + 1
     square
         & reachable terrain
         & nowNearer neighbourDist
         & map (\ (pos, (elv, _)) -> (pos, (elv, neighbourDist)))
         & foldl (\ trn (pos, val) -> Map.insert pos val trn) terrain
 
-nowNearer newDist neighbours = do
-    neighbours
-        & filter (\ neighbour -> newDist < dist neighbour)
+nowNearer newDist neighbours =
+    neighbours & filter (\ neighbour -> newDist < distance neighbour)
 
-reachable terrain square = do
+reachable terrain square =
     touching pos
-        & map (get terrain)
+        & map (readMap terrain)
         & filter (\(_, (elv, _)) -> elv <= (elevation + 1))
     where
         maxX = terrain & Map.keys & map fst & maximum
@@ -65,26 +62,15 @@ reachable terrain square = do
             [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
             & filter (\ (x, y) -> x >= 0 && x <= maxX && y >= 0 && y <= maxY)
             
-showTerrain ::  Terrain -> IO ()
-showTerrain terrain = do
-    let grid = terrain & Map.toList
-    let height = grid & map (snd . fst) & maximum & (+ 1) 
-    let cols = Split.chunksOf height grid
-    forM_ (transpose cols) $ \ x -> print x
-
-get :: Terrain -> (Int, Int) -> ((Int, Int), (Int, Int))
-get terrain pos = do 
+readMap terrain pos = do 
     let Just sqr = Map.lookup pos terrain
     (pos, sqr)
 
-dist = snd . snd
+distance (pos, (elv, d)) = d
 
-readTerrain :: [String] -> Terrain
-readTerrain lines = do
-    let values = map (map getValue) lines
-    zipWith (\ y row -> 
-            zipWith (\ x val -> ((x, y), val)) [0..] row) [0..] values
-        & concat
+readTerrain coordCharPairs = do
+    coordCharPairs
+        & map (\ (pos, char) -> (pos, getValue char))
         & Map.fromList
     where
         getValue c  
@@ -92,17 +78,19 @@ readTerrain lines = do
             | c == 'S' = (25, 2000)
             | otherwise = (25 - (Char.ord c - Char.ord 'a'), 2000)
 
-end lines = do
-    let Just (pos, _) = zipWith (\ y row -> 
-                    zipWith (\ x val -> ((x, y), val)) [0..] row) [0..] lines
-                & concat
-            & find ((== 'S'). snd)
-    pos
-
-ends lines = do
-    zipWith (\ y row -> 
-            zipWith (\ x val -> ((x, y), val)) [0..] row) [0..] lines
-        & concat
+findS coordCharPairs = 
+    coordCharPairs & find ((== 'S') . snd) & \ (Just (pos, _)) -> pos
+        
+findSorA coordsCharPairs = do
+        coordsCharPairs
         & filter ((\ elv -> elv == 'S' || elv == 'a'). snd)
+        & map fst
+
+getCoordCharPairs lines = 
+    lines
+        & zipWith 
+            (\ y row -> zipWith (\x val -> ((x, y), val)) [0 ..] row)
+            [0..]     
+        & concat
 
 pairwise a = zip a (tail a)
