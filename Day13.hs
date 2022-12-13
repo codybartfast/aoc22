27c069909d0c5d9926bee69a57cc3a87e9ac6e6a
John Donnellan <john@codybartfast.com>
2022-12-13 09:29:45 +0000

    Day 13, Part One

--- Day13.hs
+++ Day13.hs
@@ -1,7 +1,52 @@
 module Day13 (solve) where
 
+import Control.Monad (forM_)
 import Data.Function ( (&) )
 import Data.List
 
+data Msg = N Int | L [Msg] deriving (Show, Eq)
+
+instance Ord Msg where
+    compare (N a) (N b) = compare a b
+    compare (L a) (L b) = compare a b
+    compare number@(N _) list@(L _) = compare (L [number]) list
+    compare list@(L _) number@(N _) = compare list (L [number]) 
+
 solve input lines = do
-    print (take 3 lines)
+    let pairs = paras lines
+    print $
+        pairs
+        & map (\ [l1, l2] -> readMsg l1 < readMsg l2)
+        & zip [1..]
+        & filter snd
+        & map fst
+        & sum
+
+readMsg line = do
+    readList [] (tail line) & fst
+    where
+        readList :: [Msg] -> String -> (Msg, String)
+        readList parts (']':rest) = 
+            (L (reverse parts), rest)
+        readList parts (',':rest) = 
+            readList parts rest
+        readList parts txt = do
+            let (msg, rest') = readPart txt
+            readList (msg:parts) rest'
+
+        readNumber :: [Char] -> String -> (Msg, String)
+        readNumber digits (d:rest@(nxt:_))
+            | nxt == ',' || nxt == ']' = (N (read (reverse (d:digits))), rest)
+            | otherwise = readNumber (d:digits) rest
+
+        readPart :: String -> (Msg, String)
+        readPart ('[':rest) = readList [] rest
+        readPart txt = readNumber [] txt
+
+paras = 
+    paras [[]]
+    where 
+        paras :: [[String]] -> [String] -> [[String]]
+        paras grps [] = grps & map reverse & reverse
+        paras grps ("":lns) = paras ([]:grps) lns
+        paras (grp:grps) (ln:lns) = paras ((ln:grp):grps) lns        
--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@ import Day13 (solve)
 trim = List.dropWhileEnd Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day13/Test01.txt"
+  fileContent <- readFile "input/Day13/Input.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
