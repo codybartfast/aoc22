c9cf1e03132488530e4dd2a3513466196c7c604e
John Donnellan <john@codybartfast.com>
2022-12-16 11:17:46 +0000

    parse

--- Day16.hs
+++ Day16.hs
@@ -5,8 +5,22 @@ module Day16 (solve) where
 import Data.Function ( (&) )
 import Data.List
 -- import qualified Data.Char as Char
--- import qualified Data.List.Split as Split
+import qualified Data.List.Split as Split
 -- import qualified Data.Map as Map
 
+data Valve = 
+    Valve {name :: String, rate :: Int, leads :: [String]} deriving (Show)
+
 solve input lines = do
-    print $ take 3 lines
+    print $ readValves lines
+
+readValves lines = do
+    map readValve lines 
+    where
+        readValve line = do
+            let parts = Split.splitOn " " line
+            let id = parts !! 1
+            let rate = 
+                    parts !! 4 & Split.splitOn "=" & (!! 1) & init & read :: Int
+            let reachable = drop 9 parts & map (dropWhileEnd (== ','))
+            Valve {name = id, rate = rate, leads = reachable}
