3defa2a8d3ff082a1d1f0e655fa8217b230af0ab
John Donnellan <john@codybartfast.com>
2022-12-20 13:29:22 +0000

    Day 20

--- Main.hs
+++ Main.hs
@@ -2,12 +2,12 @@ import Data.List
 import qualified Data.Char as Char
 import qualified Data.List.Split as Split
 
-import Day19 (solve)
+import Day20 (solve)
 
 trim = dropWhileEnd Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day19/Input.txt"
+  fileContent <- readFile "input/Day20/Test01.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
