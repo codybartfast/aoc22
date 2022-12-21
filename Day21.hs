e833874e984b9226a584c15fc6b5b6334b538fde
John Donnellan <john@codybartfast.com>
2022-12-21 07:15:27 +0000

    Day 21

---
+++ Day21.hs
@@ -0,0 +1,13 @@
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE LambdaCase #-}
+module Day21 (solve) where
+
+import Data.Function ( (&) )
+import Data.List
+-- import qualified Data.Char as Char
+-- import qualified Data.List.Split as Split
+-- import qualified Data.Map as Map
+-- import qualified Data.Set as Set
+
+solve input lines = do
+    print $ take 3 lines
--- Main.hs
+++ Main.hs
@@ -2,12 +2,12 @@ import Data.List
 import qualified Data.Char as Char
 import qualified Data.List.Split as Split
 
-import Day20 (solve)
+import Day21 (solve)
 
 trim = dropWhileEnd Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day20/Input.txt"
+  fileContent <- readFile "input/Day21/Test01.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
