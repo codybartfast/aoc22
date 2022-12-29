47d6aa987cf1863a5dd2dfaf209faccf7e1853c9
John Donnellan <john@codybartfast.com>
2022-12-29 11:47:08 +0000

    Merry Christmas

--- Day25.hs
+++ Day25.hs
@@ -1,13 +1,56 @@
--- {-# LANGUAGE TupleSections #-}
--- {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase #-}
 module Day25 (solve) where
 
 import Data.Function ( (&) )
-import Data.List
--- import qualified Data.Char as Char
--- import qualified Data.List.Split as Split
--- import qualified Data.Map as Map
--- import qualified Data.Set as Set
 
 solve input lines = do
-    print $ take 3 lines
+    putStrLn $ foldl1 add lines
+    putStrLn "Merry Christmas"
+
+-- 'Tis the season for symbilic manipulation
+add numA numB = do
+    let len = max (length numA) (length numB)
+    let zeros = repeat '0'
+    let prep n = reverse n ++ zeros & take len
+    let (a, b) = (prep numA, prep numB)
+    foldl
+        (\ (acc, carry) (digA, digB) -> do
+                let [digCarry, digSum] = addDigits digA digB
+                let [carCarry, carSum] = addDigits digSum carry
+                let [_, carry'] = addDigits digCarry carCarry
+                (carSum : acc, carry'))
+        ("", '0')
+        (zip a b)
+    & \ case (acc, '0') -> acc; (acc, c) -> c : acc
+
+addDigits a b =
+    case (a, b) of
+        ('2', '2') -> "1-"
+        ('2', '1') -> "1="
+        ('2', '0') -> "02"
+        ('2', '-') -> "01"
+        ('2', '=') -> "00"
+
+        ('1', '2') -> "1="
+        ('1', '1') -> "02"
+        ('1', '0') -> "01"
+        ('1', '-') -> "00"
+        ('1', '=') -> "0-"
+
+        ('0', '2') -> "02"
+        ('0', '1') -> "01"
+        ('0', '0') -> "00"
+        ('0', '-') -> "0-"
+        ('0', '=') -> "0="
+
+        ('-', '2') -> "01"
+        ('-', '1') -> "00"
+        ('-', '0') -> "0-"
+        ('-', '-') -> "0="
+        ('-', '=') -> "-2"
+
+        ('=', '2') -> "00"
+        ('=', '1') -> "0-"
+        ('=', '0') -> "0="
+        ('=', '-') -> "-2"
+        ('=', '=') -> "-1"
--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import Day25 (solve)
 trim = dropWhileEnd Char.isSpace
 
 main = do
-  fileContent <- readFile "input/Day25/Test01.txt"
+  fileContent <- readFile "input/Day25/Input.txt"
   let input = trim fileContent
   let lines = Split.splitOn "\n" input
   solve input lines
